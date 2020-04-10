583d87e951a2f12324a34fe967fd60dca71bdc65
Muthu Chrish <MuthuChrish@Muthus-MacBook-Pro.local>
2020-04-10 15:28:03 -0600

    Added DemoJenkinsTrigger program

---
+++ DemoJenkinsTrigger.java
@@ -0,0 +1,18 @@
+
+public class DemoJenkinsTrigger {
+	/*if you want to run this program from command line then copy the location 
+	of this java file till src/test/java, 
+	just above the package where your java file is placed*/
+	
+	//For compiling the program in CMD line use this syntex javac packagename/Filename.java
+	//For running file in CMD use this format java packageName/Filename
+	/*if the file is placed under default package then no need to give package
+	 * name while compiling or running the java file in CMD line*/
+	 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Triggered using Jenkins Job");
+	}
+
+}
